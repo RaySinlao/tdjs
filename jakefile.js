1d9f79ac2aed69984a90224c4e1a4a2197a7bf24
Raymund Sinlao <raysinlao@Raymunds-MacBook-Pro-2.local>
2015-12-17 20:29:01 -0800

    Implemented semver to check versions

--- jakefile.js
+++ jakefile.js
@@ -1,21 +1,24 @@
-// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see 'README' or 'LICENSE' file.
 (function() {
-  "use strict";
+  'use strict';
 
-  var EXPECTED_NODE_VERSION = "v4.2.1";
+  var semver = require("semver");
 
-  desc("Default build");
-  task("default", [ "version" ], function() {
-    console.log("\n\nBUILD OK");
+  desc('Default build');
+  task('default', [ 'version' ], function() {
+    console.log('\n\nBUILD OK');
   });
 
   desc('Check Node version');
-  task("version", function(){
+  task('version', function(){
     console.log('Checking Node version: .');
+    
+    var packageJson = require('./package.json')
+    var expectedVersion = packageJson.engines.node;
 
     var actualVersion = process.version;
-    if(actualVersion !== EXPECTED_NODE_VERSION ) {
-      fail("Incorrect Node version expected " + EXPECTED_NODE_VERSION + " but was " + actualVersion);
+    if(semver.neq(expectedVersion, actualVersion)) {
+      fail('Incorrect Node version expected ' + expectedVersion + ' but was ' + actualVersion);
     }
 
   });
