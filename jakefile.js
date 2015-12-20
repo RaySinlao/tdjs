d035a1c4034dbd4d0d0868c855e60de5541f5257
Raymund Sinlao <raysinlao@Raymunds-MacBook-Pro-2.local>
2015-12-20 13:55:24 -0800

    Added testing to jakefile

--- jakefile.js
+++ jakefile.js
@@ -1,54 +1,55 @@
-// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see 'README' or 'LICENSE' file.
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 /* globals jake:false,desc:false,task:false,complete:false,fail:false */
 
 (function() {
-  'use strict';
+  "use strict";
 
   var semver = require("semver");
-  var jshint = require('simplebuild-jshint');
+  var jshint = require("simplebuild-jshint");
+  var karma = require("simplebuild-karma");
+
+  var KARMA_CONFIG = "karma.conf.js";
 
   //**** General-purpose tasks
 
   desc("Start the Karma server");
-  task('karma', function() {
-    console.log("Starting Karma server")
+  task("karma", function() {
+    karma.start({
+      configFile: KARMA_CONFIG
+    }, complete, fail);
+  }, { async: true });
+
+  desc("Default build");
+  task("default", [ "version", "lint", "test"], function() {
+    console.log("\n\nBUILD OK");
   });
 
-  desc('Default build');
-  task('default', [ 'version', 'lint' ], function() {
-    console.log('\n\nBUILD OK');
+  desc("Run a localhost server");
+  task("run", function() {
+    jake.exec("node node_modules/http-server/bin/http-server src", { interactive: true }, complete);
   });
 
-  desc('Run a localhost server');
-  task('run', function() {
-    jake.exec('node node_modules/http-server/bin/http-server src', { interactive: true }, complete);
-  });
-
-  desc('Run tests');
-  task('test', function() {
-    jake.exec('./node_modules/.bin/mocha src/test.js', { interactive: true }, complete);
-  });
-
+  
 
   //**** Supporting tasks
 
-  desc('Check Node version');
-  task('version', function() {
-    console.log('Checking Node version: .');
+  desc("Check Node version");
+  task("version", function() {
+    console.log("Checking Node version: .");
     
-    var packageJson = require('./package.json');
+    var packageJson = require("./package.json");
     var expectedVersion = packageJson.engines.node;
 
     var actualVersion = process.version;
     if(semver.neq(expectedVersion, actualVersion)) {
-      fail('Incorrect Node version expected ' + expectedVersion + ' but was ' + actualVersion);
+      fail("Incorrect Node version expected " + expectedVersion + " but was " + actualVersion);
     }
 
   });
 
-  desc('Lint the code');
-  task('lint', function() {
-    process.stdout.write('Linting Javascript: ');
+  desc("Lint the code");
+  task("lint", function() {
+    process.stdout.write("Linting Javascript: ");
 
     jshint.checkFiles({
       files: ["jakefile.js", "src/**/*.js"],
@@ -57,6 +58,15 @@
     }, complete, fail);
 
   }, { async: true });
+
+  desc("Run tests");
+  task("test", function() {
+    console.log("Testing Javascipt");
+    karma.run({
+      configFile: KARMA_CONFIG
+    }, complete, fail);
+  }, { async: true });
+
 
   function lintOptions(){
     return {
