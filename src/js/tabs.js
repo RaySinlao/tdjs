1f979961ca5be700acdeb0a8eb90d8b248f338cf
Raymund Sinlao <raysinlao@Raymunds-MacBook-Pro-2.local>
2015-12-24 15:30:01 -0800

    Added options object for tabs.initialize

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -5,10 +5,20 @@
 
   classList.shim();
 
-  exports.initialize = function initialize(elementList, className) {
-    elementList.forEach(function(element) {
-      element.classList.add(className);
+  exports.initialize = function initialize(options) {
+    var content = options.content;
+    var defaultElement = options.default;
+    var contentHideClass = options.contentHideClass;
+
+    if(content === undefined) throw new Error("Expected options.default");
+    if(defaultElement === undefined) throw new Error("Expected options.content");
+    if(contentHideClass === undefined) throw new Error("Expected options.contentHideClass");
+
+    content.forEach(function(element) {
+      element.classList.add(contentHideClass);
     });
+
+    defaultElement.classList.remove(contentHideClass);
   };
   
 })();
